807ca07a17558dcc2ad77ec820898b73861a04f1
jeremiahokyere <jeremiah.okyer3@gmail.com>
2026-05-29 14:00:53 +0200

    hide empty section on load and after adding task

--- script.js
+++ script.js
@@ -50,7 +50,33 @@ addTask.addEventListener('click', () => {
 
     tasks.push(newTask)
     renderTask(newTask)
+    checkEmptySections()
 })
+
+const checkEmptySections = () => {
+    const todayLength = document.getElementById('tasks-today').children.length
+    const laterLength = document.getElementById('tasks-later').children.length
+    const doneLength = document.getElementById('tasks-done').children.length
+    const sectionToday = document.getElementById('section-today')
+    const sectionLater = document.getElementById('section-later')
+    const sectionDone = document.getElementById('section-done')
+
+    if (todayLength == 0){
+        sectionToday.classList.add('hidden')
+    }else {
+        sectionToday.classList.remove('hidden')
+    }if (laterLength == 0){
+        sectionLater.classList.add('hidden')
+    }else {
+        sectionLater.classList.remove('hidden')
+    }if (doneLength === 0){
+        sectionDone.classList.add('hidden')
+    }else {
+        sectionDone.classList.remove('hidden')
+    }
+}
+
+checkEmptySections()
 
 
 
